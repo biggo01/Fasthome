2f30fd277f6af1ba8dcfb63e4ae528cde0e6d645
biggo01 <biggeff01@gmail.com>
2026-08-29 05:51:50 +0200

    Update views.py

--- agent_dashboard/views.py
+++ agent_dashboard/views.py
@@ -1,3 +1,23 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 
-# Create your views here.
+from visits.models import Visit
+
+
+@staff_member_required
+def dashboard(request):
+
+    pending_visits = Visit.objects.filter(
+        status=Visit.Status.PENDING
+    ).select_related(
+        "property",
+        "requester",
+    )
+
+    return render(
+        request,
+        "agent_dashboard/dashboard.html",
+        {
+            "pending_visits": pending_visits,
+        },
+    )
