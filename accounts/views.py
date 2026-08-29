823bc8a564ce6118a9b4f28709fc57f1f12d1718
biggo01 <biggeff01@gmail.com>
2026-08-29 05:34:57 +0200

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import LoginForm, RegistrationForm
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("espace_personnel:dashboard")
+
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(
+                request,
+                user,
+            )
+
+            return redirect(
+                "espace_personnel:dashboard"
+            )
+    else:
+        form = RegistrationForm()
+
+    return render(
+        request,
+        "accounts/register.html",
+        {
+            "form": form,
+        },
+    )
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("espace_personnel:dashboard")
+
+    if request.method == "POST":
+        form = LoginForm(
+            request,
+            data=request.POST,
+        )
+
+        if form.is_valid():
+            user = form.get_user()
+
+            login(
+                request,
+                user,
+            )
+
+            return redirect(
+                "espace_personnel:dashboard"
+            )
+    else:
+        form = LoginForm()
+
+    return render(
+        request,
+        "accounts/login.html",
+        {
+            "form": form,
+        },
+    )
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+
+    return redirect("public:home")
