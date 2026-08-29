b812492908f202f767f3234cd98e5161f97b032c
biggo01 <biggeff01@gmail.com>
2026-08-29 05:48:14 +0200

    Update views.py

--- public/views.py
+++ public/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from properties.models import Property
+
+
+def home(request):
+
+    featured_properties = (
+        Property.objects
+        .filter(
+            status=Property.PublicationStatus.PUBLISHED
+        )
+        .select_related("owner")[:8]
+    )
+
+    return render(
+        request,
+        "public/home.html",
+        {
+            "featured_properties": featured_properties,
+        },
+    )
